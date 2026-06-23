251407d0f2fed35b56c8f7082de91ffa64676d95
Paul Baxter <jareth_2005@yahoo.com>
2026-06-23 15:06:19 -0400

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,2 @@
+g++ -std=c++20 -I parser\ main.cpp parser\AnonLabels.cpp parser\ASTNode.cpp parser\expressionparser.cpp parser\expr_rules.cpp parser\expr_tokens.cpp parser\grammar_rule.cpp parser\opcodedict.cpp parser\parser.cpp parser\symboltable.cpp parser\tokenizer.cpp parser\utils.cpp -o pasm.exe
+ 
